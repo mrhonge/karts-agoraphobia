53e53b1ed3b1ad0c12e2616427a4c64e3bf72d5f
mrhonge <mrhonge@karts.ac.kr>
2025-05-12 01:12:05 +0900

    Fix: next.config.mjs 설정 간소화 및 react-intersection-observer 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // CSS 모듈 설정
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    });
-    return config;
-  }
+  reactStrictMode: true
 }
 
 export default nextConfig;
